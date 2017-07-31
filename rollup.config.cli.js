3777208a8f1a26e2fc81dc5749975791075c4129
churris <rene.dev.js@gmail.com>
2017-07-31 13:41:30 -0500

    Add rollup config for cli build

---
+++ rollup.config.cli.js
@@ -0,0 +1,25 @@
+import buble from 'rollup-plugin-buble';
+import json from 'rollup-plugin-json';
+import string from 'rollup-plugin-string';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+	entry: 'bin/src/index.js',
+	dest: 'bin/electron-wave',
+	format: 'cjs',
+	banner: '#!/usr/bin/env node',
+	plugins: [
+		string({
+			include: '**/*.md'
+		}),
+		json(),
+		buble(),
+		commonjs({
+			include: 'node_modules/**'
+		}),
+		nodeResolve({
+			main: true
+		})
+	]
+};
